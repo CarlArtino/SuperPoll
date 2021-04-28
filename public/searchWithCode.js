4e9b0b4762c4d0abf46d575e289630747b573ef4
michael ameteku <Ameteku@github.com>
2021-04-28 13:51:53 -0400

    Change link for getting poll

--- public/searchWithCode.js
+++ public/searchWithCode.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', e => {
     const id = {_id: document.getElementById("code-text").value};
 
     //call back end to get object from id
-    fetch('https://blooming-atoll-12908.herokuapp.com/getPoll',{
+    fetch('/getPoll',{
         method: 'post',
         body: JSON.stringify(id),
         headers: new Headers({
